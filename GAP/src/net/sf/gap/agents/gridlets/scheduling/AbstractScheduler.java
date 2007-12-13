6109e54cedb4d26ec80bff108e25d2144bb131a4
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2007-12-13 11:50:03 +0000

    [GAP] - Redesigning gridlets management

---
+++ GAP/src/net/sf/gap/agents/gridlets/scheduling/AbstractScheduler.java
@@ -0,0 +1,33 @@
+/*
+ ****************************************************************************************
+ * Copyright © Giovanni Novelli                                             
+ * All Rights Reserved.                                                                 
+ ****************************************************************************************
+ *
+ * Title:        GAP Simulator
+ * Description:  GAP (Grid Agents Platform) Toolkit for Modeling and Simulation
+ *               of Mobile Agents on Grids
+ * License:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * AbstractScheduler.java
+ *
+ * Created on 13 December 2007, 12.00 by Giovanni Novelli
+ *
+ ****************************************************************************************
+ *
+ * $Revision$
+ * $Id$
+ * $HeadURL$
+ *
+ *****************************************************************************************
+ */
+
+package net.sf.gap.agents.gridlets.scheduling;
+
+/**
+ *
+ * @author Giovanni Novelli
+ */
+public abstract class AbstractScheduler {
+
+}
