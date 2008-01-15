3765bc2463a21e9a3ac1540e46bf6601fb43d98e
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-01-15 15:58:37 +0000

    [GAP] - Gridlet submission through an additional agent queue
    Subtyping GridletList to GAPGridletList in order to better "summarize" each list

---
+++ trunk/GAP/src/net/sf/gap/agents/gridlets/GAPGridletList.java
@@ -0,0 +1,34 @@
+/*
+ ****************************************************************************************
+ * Copyright © Giovanni Novelli                                             
+ * All Rights Reserved.                                                                 
+ ****************************************************************************************
+ *
+ * Title:        GAP Simulator
+ * Description:  GAP (Grid Agents Platform) Toolkit for Modeling and Simulation
+ *               of Mobile Agents on Grids
+ * License:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * GAPGridletList.java
+ *
+ * Created on 15 January 2008, 17.00 by Giovanni Novelli
+ *
+ ****************************************************************************************
+ *
+ * $Revision$
+ * $Id$
+ * $HeadURL$
+ *
+ *****************************************************************************************
+ */
+
+package net.sf.gap.agents.gridlets;
+
+import gridsim.GridletList;
+
+/**
+ *
+ * @author Giovanni Novelli
+ */
+public class GAPGridletList extends GridletList {
+}
