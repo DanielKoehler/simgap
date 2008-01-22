af0e6bc510b799527de0e5d9ed02eee6216ae119
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-01-22 10:28:35 +0000

    [MC] - Working on XMLReader Types

---
+++ trunk/MC/src/net/sf/gap/mc/core/xml/WeekendListType.java
@@ -0,0 +1,25 @@
+/*
+ ****************************************************************************************
+ * Copyright © Giovanni Novelli                                             
+ * All Rights Reserved.                                                                 
+ ****************************************************************************************
+ *
+ * License:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * WeekendListType.java
+ *
+ * Created on 22 January 2008, 09.00 by Giovanni Novelli
+ *
+ * $Id$
+ *
+ */
+
+package net.sf.gap.mc.core.xml;
+
+/**
+ *
+ * @author Giovanni Novelli
+ */
+public class WeekendListType {
+
+}
