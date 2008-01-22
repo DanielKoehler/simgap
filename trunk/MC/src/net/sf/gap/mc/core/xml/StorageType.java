cc36a76de1567083ce54b801a7c74ea5364424ec
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-01-22 11:03:12 +0000

    [MC] - Working on XMLReader Types

---
+++ trunk/MC/src/net/sf/gap/mc/core/xml/StorageType.java
@@ -0,0 +1,24 @@
+/*
+ ****************************************************************************************
+ * Copyright © Giovanni Novelli                                             
+ * All Rights Reserved.                                                                 
+ ****************************************************************************************
+ *
+ * License:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * StorageType.java
+ *
+ * Created on 22 January 2008, 09.00 by Giovanni Novelli
+ *
+ * $Id$
+ *
+ */
+package net.sf.gap.mc.core.xml;
+
+/**
+ *
+ * @author Giovanni Novelli
+ */
+public class StorageType {
+
+}
