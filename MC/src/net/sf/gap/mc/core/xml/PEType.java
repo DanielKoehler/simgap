207713bf8db698b79bf2a6dc8a98980d8f3f514d
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-01-22 08:47:23 +0000

    [MC] - Adding XMLReader

---
+++ MC/src/net/sf/gap/mc/core/xml/PEType.java
@@ -0,0 +1,41 @@
+/*
+ ****************************************************************************************
+ * Copyright © Giovanni Novelli                                             
+ * All Rights Reserved.                                                                 
+ ****************************************************************************************
+ *
+ * License:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * PEType.java
+ *
+ * Created on 22 January 2008, 09.00 by Giovanni Novelli
+ *
+ * $Id$
+ *
+ */
+
+package net.sf.gap.mc.core.xml;
+
+/**
+ *
+ * @author Giovanni Novelli
+ */
+public class PEType {
+    private int MIPS;
+
+    public PEType() {
+        this.setMIPS(1000);
+    }
+    
+    public PEType(int MIPS) {
+        this.setMIPS(MIPS);
+    }
+    
+    public int getMIPS() {
+        return MIPS;
+    }
+
+    public void setMIPS(int MIPS) {
+        this.MIPS = MIPS;
+    }
+}
