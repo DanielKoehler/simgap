2ae9b5d38e63a4ad24ae70d8b66df5eb4966b913
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-01-22 11:22:37 +0000

    [MC] - Working on XMLReader Types

--- MC/src/net/sf/gap/mc/core/xml/StorageType.java
+++ MC/src/net/sf/gap/mc/core/xml/StorageType.java
@@ -20,5 +20,47 @@ package net.sf.gap.mc.core.xml;
  * @author Giovanni Novelli
  */
 public class StorageType {
+    private ReplicaManagerType RM;
+    private HardDiskListType HardDiskList;
+    private TapeListType TapeList;
+    
+    public StorageType(
+            String RM_name
+            ) {
+        this.setRM(new ReplicaManagerType(RM_name));
+        this.setHardDiskList(new HardDiskListType());
+        this.setTapeList(new TapeListType());
+    }
 
+    public boolean addHardDisk(HardDiskType hd) {
+        return this.getHardDiskList().addHardDisk(hd);
+    }
+    
+    public boolean addTape(TapeType tape) {
+        return this.getTapeList().addTape(tape);
+    }
+    
+    public ReplicaManagerType getRM() {
+        return RM;
+    }
+
+    public void setRM(ReplicaManagerType RM) {
+        this.RM = RM;
+    }
+
+    public HardDiskListType getHardDiskList() {
+        return HardDiskList;
+    }
+
+    public void setHardDiskList(HardDiskListType HardDiskList) {
+        this.HardDiskList = HardDiskList;
+    }
+
+    public TapeListType getTapeList() {
+        return TapeList;
+    }
+
+    public void setTapeList(TapeListType TapeList) {
+        this.TapeList = TapeList;
+    }
 }
