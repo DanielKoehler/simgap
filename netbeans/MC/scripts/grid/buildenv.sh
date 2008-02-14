47a1bc300e5538b3f0bd11e6e5d61625dab0bb31
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-14 21:15:52 +0000

    [qagesa] - Deploy MC.zip to co2#seiya#globus

--- netbeans/MC/scripts/grid/buildenv.sh
+++ netbeans/MC/scripts/grid/buildenv.sh
@@ -6,15 +6,15 @@ LOCALFILE_JRE=$(basename $REMOTEFILE_JRE)
 LOCALFILE_MC=$(basename $REMOTEFILE_MC)
 LFC_HOST=infn-se-01.ct.pi2s2.it
 echo "INFO:  Getting bzipped JRE to $PWD/$LOCALFILE_JRE from lfn:$REMOTEFILE_JRE at LFC_HOST=$LFC_HOST"
-lcg-cp --vo cometa lfn:$REMOTEFILE_JRE file://$PWD/$LOCALFILE_JRE &> /dev/null
+lcg-cp --vo cometa lfn:$REMOTEFILE_JRE file://$PWD/$LOCALFILE_JRE 
 echo "INFO:  Extracting bzipped JRE $PWD/$LOCALFILE_JRE to $PWD/java"
-rm -fR $PWD/java &> /dev/null
-tar xvjf $LOCALFILE_JRE &> /dev/null
+rm -fR $PWD/java 
+tar xvjf $LOCALFILE_JRE 
 export PATH=$PWD/java/bin:$PATH
 echo "INFO:  Getting zipped MC to $PWD/$LOCALFILE_MC from lfn:$REMOTEFILE_MC at LFC_HOST=$LFC_HOST"
-lcg-cp --vo cometa lfn:$REMOTEFILE_MC file://$PWD/$LOCALFILE_MC &> /dev/null
+lcg-cp --vo cometa lfn:$REMOTEFILE_MC file://$PWD/$LOCALFILE_MC 
 echo "INFO:  Extracting zipped MC $PWD/$LOCALFILE_MC to $PWD/MC"
-rm -fR $PWD/MC &> /dev/null
-unzip $LOCALFILE_MC -d $PWD/MC &> /dev/null
+rm -fR $PWD/MC 
+unzip $LOCALFILE_MC -d $PWD/MC 
 cd MC
 sh run.sh conf/QAGESA_64_16_MS.conf  512 512
