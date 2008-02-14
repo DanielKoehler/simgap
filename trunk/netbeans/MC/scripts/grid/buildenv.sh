d89311f6fb301e9fa00a92d3e51d768a7a00278b
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-14 16:38:52 +0000

    Updated scripts

--- trunk/netbeans/MC/scripts/grid/buildenv.sh
+++ trunk/netbeans/MC/scripts/grid/buildenv.sh
@@ -13,7 +13,8 @@ tar xvjf $LOCALFILE_JRE &> /dev/null
 export PATH=$PWD/java/bin:$PATH
 echo "INFO:  Getting zipped MC to $PWD/$LOCALFILE_MC from lfn:$REMOTEFILE_MC at LFC_HOST=$LFC_HOST"
 lcg-cp --vo cometa lfn:$REMOTEFILE_MC file://$PWD/$LOCALFILE_MC &> /dev/null
-echo "INFO:  Extracting zipped MC $PWD/$LOCALFILE_MC to $PWD/java"
+echo "INFO:  Extracting zipped MC $PWD/$LOCALFILE_MC to $PWD/MC"
 rm -fR $PWD/MC &> /dev/null
-unzip -f $LOCALFILE_MC -d $PWD/MC &> /dev/null
-
+unzip $LOCALFILE_MC -d $PWD/MC &> /dev/null
+cd MC
+sh run.sh conf/QAGESA_64_16_MS.conf  512 512
