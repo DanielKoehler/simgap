b600be3cc9bb6cf6c3957ed0bc3cab03189f4bff
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-14 16:13:05 +0000

    Updated scripts

--- netbeans/MC/scripts/grid/extract_jre.sh
+++ netbeans/MC/scripts/grid/extract_jre.sh
@@ -1,8 +1,10 @@
-REMOTEFILE=$(cat remotefile.txt)
+LOCAL_NAME=$1
+REMOTEFILE=$(cat $LOCAL_NAME)
 LOCALFILE=$(basename $REMOTEFILE)
 LFC_HOST=infn-se-01.ct.pi2s2.it
-lcg-cp --vo cometa "$REMOTEFILE" "file:///$PWD/$LOCALFILE"
-echo "extracting JVM..."
+echo "INFO:  Getting bzipped JRE to $PWD/$LOCALFILE from lfn:$REMOTEFILE at LFC_HOST=$LFC_HOST"
+lcg-cp --vo cometa lfn:$REMOTEFILE file://$PWD/$LOCALFILE
+echo "INFO:  Extracting bzipped JRE $PWD/$LOCALFILE to $PWD/java"
 tar xvjf $LOCALFILE
 export PATH=$PWD/java/bin:$PATH
 
