c4e10da9bf0fabb37cd7b0e68adfac1f28bf7121
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-14 16:31:40 +0000

    Updated scripts

--- netbeans/MC/scripts/grid/buildenv.sh
+++ netbeans/MC/scripts/grid/buildenv.sh
@@ -6,12 +6,14 @@ LOCALFILE_JRE=$(basename $REMOTEFILE_JRE)
 LOCALFILE_MC=$(basename $REMOTEFILE_MC)
 LFC_HOST=infn-se-01.ct.pi2s2.it
 echo "INFO:  Getting bzipped JRE to $PWD/$LOCALFILE_JRE from lfn:$REMOTEFILE_JRE at LFC_HOST=$LFC_HOST"
-lcg-cp --vo cometa lfn:$REMOTEFILE_JRE file://$PWD/$LOCALFILE_JRE
+lcg-cp --vo cometa lfn:$REMOTEFILE_JRE file://$PWD/$LOCALFILE_JRE &> /dev/null
 echo "INFO:  Extracting bzipped JRE $PWD/$LOCALFILE_JRE to $PWD/java"
-tar xvjf $LOCALFILE_JRE
+rm -fR $PWD/java &> /dev/null
+tar xvjf $LOCALFILE_JRE &> /dev/null
 export PATH=$PWD/java/bin:$PATH
 echo "INFO:  Getting zipped MC to $PWD/$LOCALFILE_MC from lfn:$REMOTEFILE_MC at LFC_HOST=$LFC_HOST"
-lcg-cp --vo cometa lfn:$REMOTEFILE_MC file://$PWD/$LOCALFILE_MC
+lcg-cp --vo cometa lfn:$REMOTEFILE_MC file://$PWD/$LOCALFILE_MC &> /dev/null
 echo "INFO:  Extracting zipped MC $PWD/$LOCALFILE_MC to $PWD/java"
-unzip $LOCALFILE_MC -f -d MC
+rm -fR $PWD/MC &> /dev/null
+unzip -f $LOCALFILE_MC -d $PWD/MC &> /dev/null
 
