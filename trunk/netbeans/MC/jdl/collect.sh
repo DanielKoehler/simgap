81fe3c84c34fa8a2893d4992732f137a51dbe955
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-15 15:07:35 +0000

    Collect grid output

--- trunk/netbeans/MC/jdl/collect.sh
+++ trunk/netbeans/MC/jdl/collect.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo a | edg-job-get-output -i $HOME/jid
-DATA=$(date +%Y%m%d%h%M%S)
+DATA=$(date +%Y%m%d%H%M%S)
 FRNAME=$DATA-results.tar
 if tar -cf $FRNAME /tmp/jobOutput/novelli_* &> /dev/null
 then 
