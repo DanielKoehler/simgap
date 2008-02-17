1ce66f81f08435b0cced34cb7c7359911d42bd38
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-17 11:44:51 +0000

    mem

--- trunk/netbeans/MC/jdl/ga1.5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ../MC/conf/*.conf .
-ls -l ../MC/conf | grep -v total | awk '{print "sed s/QAGESA.conf/"$9"/g run1.5g.jdl > "$9".jdl; edg-job-submit --vo cometa -o $HOME/jid "$9".jdl"'
