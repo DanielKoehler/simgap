b2489cad7d5bc3482d7e33a44165568b116096cd
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-19 11:16:01 +0000

    parametrizing jid

--- trunk/netbeans/MC/jdl/collect.sh
+++ trunk/netbeans/MC/jdl/collect.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-echo a | edg-job-get-output -i $HOME/jid
+JID=$1
+echo a | edg-job-get-output -i $JID
 DATA=$(date +%Y%m%d%H%M%S)
 FRNAME=$DATA-results.tar
 if tar -cf $FRNAME /tmp/jobOutput/novelli_* &> /dev/null
