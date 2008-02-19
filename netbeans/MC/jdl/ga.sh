d7a5e767fe4e2a1c8f6aa633cac8316351b0cc69
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-19 20:29:34 +0000

    jdl

--- netbeans/MC/jdl/ga.sh
+++ netbeans/MC/jdl/ga.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 JID=$1
-sh ga1.sh $JID
-sh ga2.sh $JID
-sh ga3.sh $JID
+sh ga1.sh $JID | sed s/\$JID/\$HOME\\/$1/g
+sh ga2.sh $JID | sed s/\$JID/\$HOME\\/$1/g
+sh ga3.sh $JID | sed s/\$JID/\$HOME\\/$1/g
+
