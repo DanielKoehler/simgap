2792768e9b93f47bd57d029c78560494a8d0c3f1
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-25 11:24:15 +0000

    scripts

--- trunk/netbeans/MC/scripts/USERS.sh
+++ trunk/netbeans/MC/scripts/USERS.sh
@@ -1,1 +1,3 @@
-(cat $1.csv | awk -F ";" '{print $3 " " $8 " " $9 " " $10 }' | sort -k 2) > $1.dat
+# Select results for USER 0
+#!/bin/sh
+(cat $1.csv | grep "_0;" | awk -F ";" '{print $3 " " $8 " " $9 " " $10 }' | sort -k 2) > $1.dat
