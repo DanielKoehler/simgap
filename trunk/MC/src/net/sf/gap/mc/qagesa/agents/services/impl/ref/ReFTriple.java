1ad59f0b77fc19b10dec98cba6c9ed2ae01d094a
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-26 00:09:40 +0000

    0.618 latency (1-0.618) load proximity

--- trunk/MC/src/net/sf/gap/mc/qagesa/agents/services/impl/ref/ReFTriple.java
+++ trunk/MC/src/net/sf/gap/mc/qagesa/agents/services/impl/ref/ReFTriple.java
@@ -37,7 +37,7 @@ public class ReFTriple implements Comparator, Comparable<ReFTriple> {
     }
 
     public ReFTriple(double load, double latency, int ceID, int seID) {
-        double aproximity = load * latency;
+        double aproximity = (load * latency) * (1.0-0.618) + latency * 0.618;
         Double aDistance = new Double(aproximity);
         ReFCouple aCouple = new ReFCouple(ceID,seID);
         this.setProximity(aDistance);
