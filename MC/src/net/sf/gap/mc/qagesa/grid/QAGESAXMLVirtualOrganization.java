40e634d297d37c2d03b1aaed5ea335d10af1e47d
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-02-29 18:45:28 +0000

    zipf

--- MC/src/net/sf/gap/mc/qagesa/grid/QAGESAXMLVirtualOrganization.java
+++ MC/src/net/sf/gap/mc/qagesa/grid/QAGESAXMLVirtualOrganization.java
@@ -441,7 +441,7 @@ public class QAGESAXMLVirtualOrganization extends AbstractVirtualOrganization {
 
     public void createAndAttachUsers() throws Exception {
         double baudrate = 4987000;
-        double delay = 4.0;
+        double delay = 10.0;
         this.setTranscodingSet(new TranscodingSet("measures/videos.csv",
                 "measures/chunks.csv"));
         @SuppressWarnings("unused")
