a75a483e8c86c196f1f4206b8ad49794112182b1
gnovelli <gnovelli@0d6af87c-5e43-0410-9a19-4da552843829>
2008-03-03 08:16:49 +0000

    results name based on conf filename

--- netbeans/MC/jdl/buildenv.sh
+++ netbeans/MC/jdl/buildenv.sh
@@ -4,6 +4,8 @@ CONF=$2
 #MEM=$(cat /proc/meminfo  | grep MemTotal | awk '{print $2"*.68"}' | bc | awk -F "." '{print $1}')
 MMIN=$3
 MMAX=$4
+SUFFIX=$(echo QAGESA_xml_RMS_1024_8_zipf_nca_re_ra.conf  | awk -F "." '{print $1}' | awk -F "QAGESA_" '{print $2}')
+RESULTSNAME=results_$SUFFIX
 REMOTEFILE_JRE=$(cat $LOCAL_JRE)
 LOCALFILE_JRE=$(basename $REMOTEFILE_JRE)
 LFC_HOST=infn-se-01.ct.pi2s2.it
@@ -16,7 +18,6 @@ export PATH=$PWD/java/bin:$PATH
 unzip MC.zip -d MC
 cd MC
 sh run.sh $CONF $MMIN $MMAX
-tar -cf results.tar results
-bzip2 -9 results.tar
-cp results.tar.bz2 ..
-
+tar -cf $SUFFIX.tar $SUFFIX
+bzip2 -9 $SUFFIX.tar
+cp $SUFFIX.tar.bz2 ..
